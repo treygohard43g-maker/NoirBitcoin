c3a188222a2e8b9a6f1b668d8d71706b8ecbd80c
treygohard43g-maker <treygohard43g@gmail.com>
2026-08-01 17:24:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -110,9 +110,6 @@ window.location.href = "login.html";
 
 // ---------- LOGIN ----------
 
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-
 const loginForm = document.getElementById("loginForm");
 
 if (loginForm) {
