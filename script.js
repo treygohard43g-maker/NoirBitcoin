eed62c692d2e1653916069afbbfd55cd8410aa1a
treygohard43g-maker <treygohard43g@gmail.com>
2026-08-01 18:25:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -122,13 +122,9 @@ if (loginForm) {
 
         e.preventDefault();
 
-        alert("Login button clicked");
-
         const email = document.getElementById("loginEmail").value.trim();
 
         const password = document.getElementById("loginPassword").value;
-
-        alert("Email: " + email);
 
         try {
             
