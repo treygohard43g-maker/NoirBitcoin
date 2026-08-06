5d4a1a1174e23f708e7850f12e5fbb8fac1d3722
treygohard43g-maker <treygohard43g@gmail.com>
2026-08-06 16:46:25 +0100

    Update search.js

--- search.js
+++ search.js
@@ -47,7 +47,7 @@ async function searchCoins(query) {
 
 }
 
-function displayResults(coins) {
+async function displayResults(coins) {
 
     searchResults.innerHTML = "";
 
@@ -60,6 +60,52 @@ function displayResults(coins) {
         return;
 
     }
+
+    for (const coin of coins.slice(0, 10)) {
+
+        let price = "Loading...";
+
+        try {
+
+            const res = await fetch(
+                `https://api.coingecko.com/api/v3/simple/price?ids=${coin.id}&vs_currencies=usd`,
+                {
+                    headers: {
+                        "x-cg-demo-api-key": COINGECKO_API_KEY
+                    }
+                }
+            );
+
+            const data = await res.json();
+
+            if (data[coin.id]) {
+                price = "$" + data[coin.id].usd.toLocaleString();
+            }
+
+        } catch (e) {}
+
+        searchResults.innerHTML += `
+            <div class="search-item" data-id="${coin.id}">
+
+                <img src="${coin.thumb}" class="search-logo">
+
+                <div class="search-info">
+                    <h4>${coin.name}</h4>
+                    <span>${coin.symbol.toUpperCase()}</span>
+                </div>
+
+                <div class="search-price">
+                    ${price}
+                </div>
+
+            </div>
+        `;
+
+    }
+
+    searchResults.style.display = "block";
+
+}
 
     coins.slice(0, 10).forEach((coin) => {
 
