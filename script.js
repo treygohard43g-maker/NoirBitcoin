ec0b429307f750fc91ca05ef480ae476e98dcc8a
treygohard43g-maker <treygohard43g@gmail.com>
2026-08-23 13:33:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ import {
     signInWithEmailAndPassword,
     EmailAuthProvider,
     reauthenticateWithCredential,
-    updatePassword
+    updatePassword,
     onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
