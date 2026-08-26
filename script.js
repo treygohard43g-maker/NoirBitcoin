b3464d8314f9d6323efceaf24354fae945969e72
treygohard43g-maker <treygohard43g@gmail.com>
2026-08-26 13:02:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1055,7 +1055,7 @@ if (chartContainer && typeof LightweightCharts !== "undefined") {
     const chart = LightweightCharts.createChart(chartContainer, {
 
         width: chartContainer.clientWidth,
-        height: 90,
+        height: chartContainer.clientHeight,
 
         layout: {
             background: {
