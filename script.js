fa8ba6bb78cdea586a1c786e43036162ab7ba71e
treygohard43g-maker <treygohard43g@gmail.com>
2026-08-28 10:05:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1371,7 +1371,7 @@ async function loadBitcoinPrice() {
     try {
 
         const response = await fetch(
-        ""https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_24hr_change=true"
+          "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_24hr_change=true"
         );
 
         const data = await response.json();
