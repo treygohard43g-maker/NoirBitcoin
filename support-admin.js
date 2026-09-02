c9501d34495a18c3c3193cfc4c03d7b0901fddc3
treygohard43g-maker <treygohard43g@gmail.com>
2026-09-02 22:07:52 +0100

    Update support-admin.js

--- support-admin.js
+++ support-admin.js
@@ -8,7 +8,9 @@ import {
     orderBy,
     onSnapshot,
     addDoc,
-    serverTimestamp
+    serverTimestamp,
+    updateDoc,
+    doc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const db = getFirestore(auth.app);
