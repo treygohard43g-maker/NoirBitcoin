600062a241cb9483d8660957445ff3b6cc57a8cd
treygohard43g-maker <treygohard43g@gmail.com>
2026-09-02 18:22:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -318,7 +318,7 @@ if (loginForm) {
                 "Firebase login successful:",
                 user.uid
             );
-
+            await recordLoginActivity(user);
             const name =
                 user.displayName || "User";
 
